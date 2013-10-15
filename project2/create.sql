ee3b2c045836a8274e132b91f100add99fad2020
rfang <rfang@ucla.edu>
2013-10-14 17:37:59 -0700

    Filled in create.sql mistakes

--- project2/create.sql
+++ project2/create.sql
@@ -1,5 +1,5 @@
 CREATE TABLE User (
-	UserID			VARCHAR(100) Primary Key NOT NULL,
+	UserID			VARCHAR(400) Primary Key NOT NULL,
 	Rating			INT NOT NULL,
 	Location		VARCHAR(100) NOT NULL,
 	Country			VARCHAR(100) NOT NULL
@@ -7,24 +7,24 @@ CREATE TABLE User (
 
 CREATE TABLE Item (
 	ItemID 				INT Primary Key NOT NULL,
-	Name				VARCHAR(4000),
+	Name				VARCHAR(400),
 	Currently			DECIMAL(8,2),
 	Buy_Price			DECIMAL(8,2),
 	First_Bid			DECIMAL(8,2) NOT NULL,
 	Started				TIMESTAMP,
 	Seller				ForeignKey(User),
 	Ends				TIMESTAMP,
-	Description			VARCHAR(4000),
+	Description			VARCHAR(4000)
 );
 
 CREATE TABLE ItemCategory ( 
 	ItemID 		ForeignKey(Item) Primary Key NOT NULL,
-	Category	VARCHAR(?) Primary Key NOT NULL
+	Category	VARCHAR(400) Primary Key NOT NULL
 );
 
 CREATE TABLE ItemBid ( 
 	ItemID 		ForeignKey(Item) Primary Key NOT NULL,
 	Bidder		ForeignKey(User) Primary Key NOT NULL,
 	Time		TIMESTAMP Primary Key NOT NULL,
-	Amount		VARCHAR(?)
+	Amount		DECIMAL(8,2)
 );
