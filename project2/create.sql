c4b437c1907f0a45e29bd986ec0b51f5c7d7cf51
rfang <rfang@ucla.edu>
2013-10-14 18:51:57 -0700

    Fix Foreign Key

--- project2/create.sql
+++ project2/create.sql
@@ -6,25 +6,25 @@ CREATE TABLE User (
 );
 
 CREATE TABLE Item (
-	ItemID 				INT Primary Key NOT NULL,
-	Name				VARCHAR(400),
-	Currently			DECIMAL(8,2),
-	Buy_Price			DECIMAL(8,2),
-	First_Bid			DECIMAL(8,2) NOT NULL,
-	Started				TIMESTAMP,
-	Seller				ForeignKey(User),
-	Ends				TIMESTAMP,
-	Description			VARCHAR(4000)
+	ItemID 			INT Primary Key NOT NULL,
+	Name			VARCHAR(400),
+	Currently		DECIMAL(8,2),
+	Buy_Price		DECIMAL(8,2),
+	First_Bid		DECIMAL(8,2) NOT NULL,
+	Started			TIMESTAMP,
+	Seller			ForeignKey(UserID) references User.UserID,
+	Ends			TIMESTAMP,
+	Description		VARCHAR(4000)
 );
 
 CREATE TABLE ItemCategory ( 
-	ItemID 		ForeignKey(Item) Primary Key NOT NULL,
+	ItemID 		ForeignKey(ItemID) references Item(ItemID) Primary Key NOT NULL,
 	Category	VARCHAR(400) Primary Key NOT NULL
 );
 
 CREATE TABLE ItemBid ( 
-	ItemID 		ForeignKey(Item) Primary Key NOT NULL,
-	Bidder		ForeignKey(User) Primary Key NOT NULL,
+	ItemID 		ForeignKey(ItemID) references Item(ItemID) Primary Key NOT NULL,
+	Bidder		ForeignKey(UserID) references User(UserID)Primary Key NOT NULL,
 	Time		TIMESTAMP Primary Key NOT NULL,
 	Amount		DECIMAL(8,2)
 );
