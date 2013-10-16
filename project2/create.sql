0b18e1e113f7b99528781f593ec8b629f42dd1fc
rfang <rfang@ucla.edu>
2013-10-16 14:21:25 -0700

    switched intends to spaces.

--- project2/create.sql
+++ project2/create.sql
@@ -1,29 +1,28 @@
 CREATE TABLE User (
-	UserID			VARCHAR(400) Primary Key NOT NULL,
-	Rating			INT NOT NULL,
-	Location		VARCHAR(100),
-	Country			VARCHAR(100)
-);
+    UserID          VARCHAR(400) Primary Key NOT NULL,
+    Rating          INT NOT NULL,
+    Location        VARCHAR(100),
+    Country         VARCHAR(100)
 
 CREATE TABLE Item (
-	ItemID 			INT Primary Key NOT NULL,
-	Name			VARCHAR(400),
-	Buy_Price		DECIMAL(8,2),
-	First_Bid		DECIMAL(8,2) NOT NULL,
-	Started			TIMESTAMP,
-	Seller			ForeignKey(UserID) references User.UserID,
-	Ends			TIMESTAMP,
-	Description		VARCHAR(4000)
+    ItemID          INT Primary Key NOT NULL,
+    Name            VARCHAR(400),
+    Buy_Price       DECIMAL(8,2),
+    First_Bid       DECIMAL(8,2) NOT NULL,
+    Started         TIMESTAMP,
+    Seller          ForeignKey(UserID) references User.UserID,
+    Ends            TIMESTAMP,
+    Description     VARCHAR(4000)
 );
 
 CREATE TABLE ItemCategory ( 
-	ItemID 		ForeignKey(ItemID) references Item(ItemID) Primary Key NOT NULL,
-	Category	VARCHAR(400) Primary Key NOT NULL
+    ItemID          ForeignKey(ItemID) references Item(ItemID) Primary Key NOT NULL,
+    Category        VARCHAR(400) Primary Key NOT NULL
 );
 
 CREATE TABLE ItemBid ( 
-	ItemID 		ForeignKey(ItemID) references Item(ItemID) Primary Key NOT NULL,
-	Bidder		ForeignKey(UserID) references User(UserID)Primary Key NOT NULL,
-	Time		TIMESTAMP Primary Key NOT NULL,
-	Amount		DECIMAL(8,2)
+    ItemID          ForeignKey(ItemID) references Item(ItemID) Primary Key NOT NULL,
+    Bidder          ForeignKey(UserID) references User(UserID)Primary Key NOT NULL,
+    Time            TIMESTAMP Primary Key NOT NULL,
+    Amount          DECIMAL(8,2)
 );
