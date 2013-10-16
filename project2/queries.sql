124c92322bb724bb3354f31902fa910bef03db5a
rfang <rfang@ucla.edu>
2013-10-16 14:22:03 -0700

    converted indents to spaces

--- project2/queries.sql
+++ project2/queries.sql
@@ -4,31 +4,31 @@ SELECT COUNT(*) FROM User;
 #num sellers where location is New York
 SELECT COUNT(*) 
 FROM (SELECT Seller FROM Item) AS I 
-	INNER JOIN User
-	ON I.Seller=User.UserID
+    INNER JOIN User
+    ON I.Seller=User.UserID
 WHERE User.Location="New York";
 
 # auctions with four categories
 SELECT COUNT(*) 
 FROM 
-	(
-		SELECT ItemID, COUNT(Category) 
-		FROM ItemCategory
-		GROUP BY ItemID
-		HAVING COUNT(Category) = 4
-	) AS Auctions;
+    (
+        SELECT ItemID, COUNT(Category) 
+        FROM ItemCategory
+        GROUP BY ItemID
+        HAVING COUNT(Category) = 4
+    ) AS Auctions;
 
 # ID with highest bid, unsold 
 SELECT DISTINCT ItemID
 FROM
-	(
-		SELECT MAX(Amount) AS Amount
-		FROM Item INNER JOIN ItemBid 
-		USING (ItemID)
-		WHERE Started <  '2012-12-01 00:00:01' AND Ends > '2012-12-01 00:00:01');
-	) AS Highest_Bid 
-	INNER JOIN ItemBid 
-	USING (Amount);
+    (
+        SELECT MAX(Amount) AS Amount
+        FROM Item INNER JOIN ItemBid 
+        USING (ItemID)
+        WHERE Started <  '2012-12-01 00:00:01' AND Ends > '2012-12-01 00:00:01');
+    ) AS Highest_Bid 
+    INNER JOIN ItemBid 
+    USING (Amount);
 
 #num sellers
 SELECT COUNT(seller) 
@@ -41,6 +41,4 @@ FROM ItemCategory INNER JOIN ItemBid
 USING (ItemID)
 GROUP BY Category
 Having MAX(Amount)>100.00; 
-
-
 
