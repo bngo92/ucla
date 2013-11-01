6efbf87f32a9d73380c19acf2ddf7184f314c92a
rfang <rfang@ucla.edu>
2013-11-01 15:27:28 -0700

    Update Indexer.java

--- cs144/project3/src/Indexer.java
+++ cs144/project3/src/Indexer.java
@@ -1,3 +1,4 @@
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -15,49 +16,83 @@ public class Indexer {
     /** Creates a new instance of Indexer */
     public Indexer() {
     }
- 
-    public void rebuildIndexes() {
 
-        Connection conn = null;
+    private IndexWriter indexWriter = null;
+
+    public IndexWriter getIndexWriter(boolean create) throws IOException {
+        if (indexWriter == null) {
+            indexWriter = new IndexWriter("index-directory",new StandardAnalyzer(), create);
+        }
+        return indexWriter;
+    }
+
+    public void closeIndexWriter() throws IOException {
+        if (indexWriter != null) {
+            indexWriter.close();
+        }
+    }
+
+    public void rebuildIndexes() throws IOException {
+
+        getIndexWriter(true);
+
+        Connection conn;
+        Statement stmt;
 
         // create a connection to the database to retrieve Items from MySQL
-	try {
-	    conn = DbManager.getConnection(true);
-	} catch (SQLException ex) {
-	    System.out.println(ex);
-	}
+        try {
+            conn = DbManager.getConnection(true);
+            stmt = conn.createStatement();
+        } catch (SQLException ex) {
+            System.out.println(ex);
+            return;
+        }
 
 
-	/*
-	 * Add your code here to retrieve Items using the connection
-	 * and add corresponding entries to your Lucene inverted indexes.
-         *
-         * You will have to use JDBC API to retrieve MySQL data from Java.
-         * Read our tutorial on JDBC if you do not know how to use JDBC.
-         *
-         * You will also have to use Lucene IndexWriter and Document
-         * classes to create an index and populate it with Items data.
-         * Read our tutorial on Lucene as well if you don't know how.
-         *
-         * As part of this development, you may want to add 
-         * new methods and create additional Java classes. 
-         * If you create new classes, make sure that
-         * the classes become part of "edu.ucla.cs.cs144" package
-         * and place your class source files at src/edu/ucla/cs/cs144/.
-	 * 
-	 */
 
+        ResultSet rs;
+        try {
+            rs = stmt.executeQuery("SELECT ItemID, Name, Description FROM Item");
+            while(rs.next()) {
+                String itemID = rs.getInt("ItemID")+"";
+                IndexWriter writer = getIndexWriter(false);
+                Document doc = new Document();
+                doc.add(new Field("ID", itemID, Field.Store.YES, Field.Index.NO));
+                String name = rs.getString("Name");
+                doc.add(new Field("Name", name, Field.Store.YES, Field.Index.TOKENIZED));
+                String description = rs.getString("Description");
+                doc.add(new Field("Description", description, Field.Store.YES, Field.Index.TOKENIZED));
 
+                ResultSet cats = stmt.executeQuery("SELECT Category FROM ItemCategory WHERE itemID="+itemID);
+                String category = "";
+                while(cats.next()) {
+                    category = category +  " " + cats.getString("Category");
+                }
+                doc.add(new Field("Category", category, Field.Store.NO, Field.Index.TOKENIZED));
+
+                doc.add(new Field("Content", name+category+description, Field.Store.NO, Field.Index.TOKENIZED));
+
+                writer.addDocument(doc);
+
+            }
+        } catch (SQLException e) {
+            System.out.println(e);
+            return;
+        }
         // close the database connection
-	try {
-	    conn.close();
-	} catch (SQLException ex) {
-	    System.out.println(ex);
-	}
-    }    
+        try {
+            conn.close();
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        }
+    }
 
     public static void main(String args[]) {
         Indexer idx = new Indexer();
-        idx.rebuildIndexes();
-    }   
+        try {
+            idx.rebuildIndexes();
+        } catch (IOException e) {
+
+        }
+    }
 }
