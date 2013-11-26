eda26936d9e12cf3aa56d9682db7be4e93b21661
Bryan Ngo <bngo92@gmail.com>
2013-11-25 20:38:59 -0800

    Added proxy servlet

--- cs144/project4a/src/edu/ucla/cs/cs144/ProxyServlet.java
+++ cs144/project4a/src/edu/ucla/cs/cs144/ProxyServlet.java
@@ -1,12 +1,16 @@
 package edu.ucla.cs.cs144;
 
-import java.io.IOException;
-import java.net.HttpURLConnection;
 import javax.servlet.Servlet;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Scanner;
 
 public class ProxyServlet extends HttpServlet implements Servlet {
        
@@ -14,6 +18,11 @@ public class ProxyServlet extends HttpServlet implements Servlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
-        // your codes here
+        HttpURLConnection connection = (HttpURLConnection) new URL("http://google.com/complete/search?output=toolbar&q=" + request.getParameter("q")).openConnection();
+        InputStream in = new BufferedInputStream(connection.getInputStream());
+        response.setContentType("text/xml");
+        response.getWriter().write(new Scanner(in).useDelimiter("\\A").next());
+        connection.disconnect();
+        in.close();
     }
 }
