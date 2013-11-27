cbc91917d3e521071c9ad4fbd6c82e795e69c85d
Bryan Ngo <bngo92@gmail.com>
2013-11-26 17:30:52 -0800

    Check for empty string

--- cs144/project4a/src/edu/ucla/cs/cs144/ItemServlet.java
+++ cs144/project4a/src/edu/ucla/cs/cs144/ItemServlet.java
@@ -1,26 +1,24 @@
 package edu.ucla.cs.cs144;
 
-import java.io.*;
-import java.text.NumberFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.*;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.FactoryConfigurationError;
-import javax.xml.parsers.ParserConfigurationException;
-
-import org.w3c.dom.*;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.Text;
 import org.xml.sax.InputSource;
-import org.xml.sax.SAXException;
-import org.xml.sax.SAXParseException;
-import org.xml.sax.ErrorHandler;
 
 import javax.servlet.Servlet;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.IOException;
+import java.io.StringReader;
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 public class ItemServlet extends HttpServlet implements Servlet {
 
@@ -104,6 +102,8 @@ public class ItemServlet extends HttpServlet implements Servlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String result = AuctionSearchClient.getXMLDataForItemId(request.getParameter("id"));
+        if (result.isEmpty())
+            return;
         Document doc = null;
         try {
             DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
