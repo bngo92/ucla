863a4609dd3e056919f2bec565eb9dada9db0082
Alexander Ramirez <adr2370@gmail.com>
2013-12-04 15:06:15 -0800

    spill last

--- cs132/hw4/src/V2VM.java
+++ cs132/hw4/src/V2VM.java
@@ -83,7 +83,12 @@ public class V2VM extends VInstr.Visitor<Throwable> {
                     continue;
                 }
 
-                String register = String.format("$t%d", last);
+                String register;
+                if (last < 9) {
+                    register = String.format("$t%d", last);
+                } else {
+                    register = String.format("$t%d", last - 9);
+                }
                 Liveness.Thing saved = registerMapBuilder.get(register);
                 if (saved == null || thing.range.start >= saved.range.end) {
                     registerMap.put(thing.var, register);
