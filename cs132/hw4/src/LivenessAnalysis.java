74c999c1bc9c5ab14ea97a67bf40394b4db08326
Bryan Ngo <bngo92@gmail.com>
2013-12-05 01:45:04 -0800

    Forgot register

--- cs132/hw4/src/LivenessAnalysis.java
+++ cs132/hw4/src/LivenessAnalysis.java
@@ -19,7 +19,7 @@ public class LivenessAnalysis extends VInstr.Visitor<Throwable> {
 
     public LivenessAnalysis(VFunction function) throws Throwable {
         this.function = function;
-        calleeRegisters.addAll(Arrays.asList("$s1", "$s2", "$s3", "$s4", "$s5", "$s6", "$s7"));
+        calleeRegisters.addAll(Arrays.asList("$s0", "$s1", "$s2", "$s3", "$s4", "$s5", "$s6", "$s7"));
         callerRegisters.addAll(Arrays.asList("$t0", "$t1", "$t2", "$t3", "$t4", "$t5", "$t6", "$t7", "$t8"));
     }
 
